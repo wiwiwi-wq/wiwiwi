fde3b660935f153f9f3714490d53d195dd3aa326
wiwiwi-wq <wiwiwi1793@gmail.com>
2025-09-10 14:44:19 +0300

    Update peremennie 1 practika.sql

--- peremennie 1 practika.sql
+++ peremennie 1 practika.sql
@@ -135,3 +135,161 @@ DECLARE @moneyA Money = 894695.8;
 PRINT @moneyA;
 DECLARE @moneyD Money = 1.98498;
 PRINT @moneyD;
+
+-------
+PRINT 'Char'
+
+DECLARE @char char(5)= 'Hello';
+PRINT @char;
+DECLARE @chara char(8)= 'ilovecat';
+PRINT @chara;
+DECLARE @charw char(2)= 'okay';
+PRINT @charw;
+DECLARE @chare char(4)= 'puppy';
+PRINT @chare;
+DECLARE @charf char(10)= 'afafafafafafafafafafaf';
+PRINT @charf;
+--------------
+PRINT 'VarChar'
+
+DECLARE @vchar varchar(max)='Hello';
+PRINT @vchar;
+DECLARE @vchara varchar(52)='HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello';
+PRINT @vchara;
+DECLARE @vcharc varchar(2)='Hello';
+PRINT @vcharc;
+DECLARE @vcharv varchar(5)='Hellofff';
+PRINT @vcharv;
+DECLARE @vcharq varchar(max)='HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello';
+PRINT @vcharq;
+----------
+PRINT 'NChar'
+
+DECLARE @Nchar Nchar(7) = 'Привет!Привет!';
+PRINT @Nchar;
+DECLARE @Nchara Nchar(2) = 'ялквмом';
+PRINT @Nchara;
+DECLARE @Nchars Nchar(19) = 'потеряли себя нема нам не надо мана';
+PRINT @Nchars;
+DECLARE @Ncharw Nchar(25) = 'команда магов вабанк и зомби там битом игриво так по чему то';
+PRINT @Ncharw;
+DECLARE @Nchart Nchar(8) = 'Привет!йоу май нейм ис вика';
+PRINT @Nchart;
+-----
+PRINT 'NvarChar'
+
+DECLARE @NvarChar NvarChar(max) = 'Привет Мир!';
+PRINT @NvarChar;
+DECLARE @NvarChara NvarChar(8) = 'обернись мне не ';
+PRINT @NvarChara;
+DECLARE @NvarChars NvarChar(30) = 'встать без твоей руки';
+PRINT @NvarChars;
+DECLARE @NvarCharf NvarChar(max) = 'ама рок стар';
+PRINT @NvarCharf;
+DECLARE @NvarChare NvarChar(52) = 'поп старпоп старпоп старпоп старпоп стар';
+PRINT @NvarChare;
+-----
+PRINT 'DateTime'
+
+
+DECLARE @datetime DateTime = CAST('2007-05-08 12:35:29.123' AS DateTime);
+PRINT @datetime;  
+DECLARE @datetimea DateTime = CAST('2000-07-04 15:52:52.123' AS DateTime);
+PRINT @datetimea;  
+DECLARE @datetimee DateTime = CAST('1994-22-01 08:59:59.123' AS DateTime);
+PRINT @datetimee;  
+DECLARE @datetimes DateTime = CAST('1753-10-12 12:00:00.123' AS DateTime);
+PRINT @datetimes;  
+DECLARE @datetimeq DateTime = CAST('9999-01-01 00:00:00.123' AS DateTime);
+PRINT @datetimeq;  
+-----
+PRINT 'DateTime2';
+
+DECLARE @datetime2 DateTime2(5) = CAST('2007-05-08 12:35:29.1234567' AS DateTime2)
+PRINT @datetime2;
+DECLARE @datetime2a DateTime2(7) = CAST('0052-05-02 05:02:52.1234567' AS DateTime2)
+PRINT @datetime2a;
+DECLARE @datetime2q DateTime2(3) = CAST('0856-05-08 12:35:29.1234567' AS DateTime2)
+PRINT @datetime2q;
+DECLARE @datetime2w DateTime2(5) = CAST('2007-05-08 12:35:29.1234567' AS DateTime2)
+PRINT @datetime2w;
+DECLARE @datetime2s DateTime2(1) = CAST('2007-05-08 12:35:29.1234567' AS DateTime2)
+PRINT @datetime2s;
+----
+PRINT 'SmallDateTime';
+
+DECLARE @smalldatetime SmallDateTime = CAST('2007-05-08 12:35:29' AS SmallDateTime)
+PRINT @smalldatetime; 
+DECLARE @smalldatetimeq SmallDateTime = CAST('2000-30-11 19:52:29' AS SmallDateTime)
+PRINT @smalldatetimeq;
+DECLARE @smalldatetimew SmallDateTime = CAST('2075-05-02 03:35:29' AS SmallDateTime)
+PRINT @smalldatetimew;
+DECLARE @smalldatetimee SmallDateTime = CAST('2052-07-07 07:07:29' AS SmallDateTime)
+PRINT @smalldatetimee;
+DECLARE @smalldatetimer SmallDateTime = CAST('2007-05-08 12:35:29' AS SmallDateTime)
+PRINT @smalldatetimer;
+----
+PRINT 'DateTimeOffSet';
+
+DECLARE @dateTimeOffSet DateTimeOffSet(5) = CAST('2007-05-08 12:35:29.1234567 +12:15' AS DateTimeOffSet)
+PRINT @dateTimeOffSet;  
+DECLARE @dateTimeOffSeta DateTimeOffSet(2) = CAST('0052-05-08 10:10:29.1234567 +12:15' AS DateTimeOffSet)
+PRINT @dateTimeOffSeta; 
+DECLARE @dateTimeOffSets DateTimeOffSet(7) = CAST('2999-02-09 01:57:29.1234567 +12:15' AS DateTimeOffSet)
+PRINT @dateTimeOffSets; 
+DECLARE @dateTimeOffSetd DateTimeOffSet(6) = CAST('1579-10-03 11:58:29.1234567 +12:15' AS DateTimeOffSet)
+PRINT @dateTimeOffSetd; 
+DECLARE @dateTimeOffSetf DateTimeOffSet(1) = CAST('0522-01-02 14:51:29.1234567 +12:15' AS DateTimeOffSet)
+PRINT @dateTimeOffSetf; 
+---
+PRINT 'Date';
+
+DECLARE @date Date = CAST('2007-05-08 12:35:29.1234567' AS Date)
+PRINT @date;  
+DECLARE @datea Date = CAST('2200-09-09 19:52:29.1234567' AS Date)
+PRINT @datea;
+DECLARE @dates Date = CAST('2075-05-02 03:35:29' AS Date)
+PRINT @dates;
+DECLARE @datee Date = CAST('2017-07-05 12:35:29.1234567' AS Date)
+PRINT @datee;
+DECLARE @datef Date = CAST('4971-07-07 12:35:29.1234567' AS Date)
+PRINT @datef;
+----
+PRINT 'Time';
+
+DECLARE @time Time(5) = CAST('2007-05-08 12:35:29.1234567' AS Time);
+PRINT @time;  
+DECLARE @timeq Time(4) = CAST('6444-04-08 15:53:29.1234567' AS Time);
+PRINT @timeq;  
+DECLARE @timew Time(7) = CAST('8456-05-05 21:21:29.1234567' AS Time);
+PRINT @timew;  
+DECLARE @timee Time(5) = CAST('2000-07-09 07:39:29.1234567' AS Time);
+PRINT @timee;  
+DECLARE @timer Time(5) = CAST('5252-07-01 05:25:29.1234567' AS Time);
+PRINT @timer;  
+---
+PRINT 'Binary'
+
+DECLARE @binary Binary(1) = 16;
+PRINT @binary;
+DECLARE @binarys Binary(548) = 47;
+PRINT @binarys;
+DECLARE @binaryq Binary(52) = 52;
+PRINT @binaryq;
+DECLARE @binarye Binary(6666) = 2;
+PRINT @binarye;
+DECLARE @binaryf Binary(3) = 52;
+PRINT @binaryf;
+---
+PRINT 'VarBinary'
+
+DECLARE @varBinary VarBinary(max) = 2147483647;
+PRINT @varBinary;
+DECLARE @varBinaryq VarBinary(7777) = 525252;
+PRINT @varBinaryq;
+DECLARE @varBinarya VarBinary(52) = 4;
+PRINT @varBinarya;
+DECLARE @varBinaryw VarBinary(5) = 52;
+PRINT @varBinaryw;
+DECLARE @varBinarye VarBinary(1) = 64;
+PRINT @varBinarye;
