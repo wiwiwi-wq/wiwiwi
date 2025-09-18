2a1a24bf2ab6b4bb1a4268948a0ddea3d872525f
wiwiwi-wq <wiwiwi1793@gmail.com>
2025-09-18 11:53:10 +0300

    Add files via upload

---
+++ 4.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        //4
+        {
+            //double a = 3, b = 3.5, c = 2.1;
+
+
+            //if (a + b > c && a + c > b && b + c > a)
+            //    {
+            //        Console.WriteLine("треугольник существует");
+            //    }
+            //if (Math.Abs(a * a - (b * b + c * c)) < 0.0001 ||
+            //    Math.Abs(b * b - (a * a + c * c)) < 0.0001 ||
+            //    Math.Abs(c * c - (a * a + b * b)) < 0.0001)
+            //{
+            //    Console.WriteLine("треугольник прямоугольный");
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("треугольник не прямоугольный");
+            //}
+
+
+            //Console.ReadKey();
+
+
+
+            //double a = 21, b = 6.55, c = 0.1;
+
+
+            //if (a + b > c && a + c > b && b + c > a)
+            //{
+            //    Console.WriteLine("треугольник существует");
+            //}
+            //if (Math.Abs(a * a - (b * b + c * c)) < 0.0001 ||
+            //    Math.Abs(b * b - (a * a + c * c)) < 0.0001 ||
+            //    Math.Abs(c * c - (a * a + b * b)) < 0.0001)
+            //{
+            //    Console.WriteLine("треугольник прямоугольный");
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("треугольник не прямоугольный");
+            //}
+
+            //Console.ReadKey();
+
+            //double a = -9, b = 3.7, c = 0.1;
+
+
+            //if (a + b > c && a + c > b && b + c > a)
+            //{
+            //    Console.WriteLine("треугольник существует");
+            //}
+            //if (Math.Abs(a * a - (b * b + c * c)) < 0.0001 ||
+            //    Math.Abs(b * b - (a * a + c * c)) < 0.0001 ||
+            //    Math.Abs(c * c - (a * a + b * b)) < 0.0001)
+            //{
+            //    Console.WriteLine("треугольник прямоугольный");
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("треугольник не прямоугольный");
+            //}
+
+            //Console.ReadKey();
+
+            ////////////////////
+            //5
+            //Console.WriteLine("введите номер месяца:");
+            //int month = int.Parse(Console.ReadLine());
+
+            //if (month == 12 || month == 1 || month == 2)
+            //{
+            //    Console.WriteLine("зима 12,1,2");
+            //}
+            //else if (month >= 3 && month <= 5)
+            //{
+            //    Console.WriteLine("весна 3,4,5");
+            //}
+            //else if (month >= 6 && month <= 8)
+            //{
+            //    Console.WriteLine("лето 6, 7, 8");
+            //}
+            //else if (month >= 9 && month <= 11)
+            //{
+            //    Console.WriteLine("осень 9,10,11");
+            //}
+
+            //Console.ReadKey();
+
+           ////////////
+
+        }
+    }
+}
